5b6e656316000e539dc872cf568c239ae506a1d3
Stephan <kutzner.stephan@uni-kassel.de>
2018-06-10 10:33:11 +0200

    initial commit

---
+++ link_files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+read -p "Install zsh? [yN] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "$ sudo apt install zsh"
+    sudo apt install zsh
+    chsh -s $(which zsh)
+
+
+read -p "Install fonts-powerline? [yN] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "$ sudo apt install fonts-powerline"
+    sudo apt install fonts-powerline
+fi
+
+ln -fsn `pwd`/.vim $HOME/.vim
+ln -fsn `pwd`/.config/fontconfig $HOME/.config/fontconfig
+ln -fs `pwd`/.vimrc $HOME/.vimrc
+ln -fs `pwd`/.gvimrc $HOME/.gvimrc
+ln -fs `pwd`/.zshrc $HOME/.zshrc
+ln -fs `pwd`/.gitconfig $HOME/.gitconfig
+mkdir -p $HOME/.local/share/fonts/
+ln -fs `pwd`/powerlevel-fonts/*.ttf $HOME/.local/share/fonts/
+fc-cache -fv
